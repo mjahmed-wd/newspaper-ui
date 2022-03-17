bfb6f573ffaba5edf1f55a7b53e61b1b350c9bcb
Jubair <mjahmed.wd@gmail.com>
2022-03-18 02:24:19 +0600

    components made

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "./assets/styles/styles.scss";
-import newsInfo from "./assets/data/data.json"
+import Header from "./components/Header";
+import LandingPage from "./pages/Landing/Landing";
 
 const App = () => {
+
   return (
-    <div>
-      <h1>Hi</h1>
-      {newsInfo.map(news =>
-        <div key={news?.id}>
-          <h2>{news?.title}</h2>
-          <p>{news?.description}</p>
-        </div>
-      )}
-    </div>
+    <>
+      <Header />
+      <LandingPage />
+    </>
   )
 }
 
